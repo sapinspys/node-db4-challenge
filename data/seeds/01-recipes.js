2ff98dbfabc1dcc3d706bba6253286f03144224c
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-07-18 13:19:55 -0400

    Populated recipes seed

--- data/seeds/01-recipes.js
+++ data/seeds/01-recipes.js
@@ -1,13 +1,7 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex("recipes").insert([
+    { id: 1, name: "Chicken Alfredo", created_by: 'Guillermo Arria-Devoe' },
+    { id: 2, name: "Mushroom Pizza", created_by: 'Guillermo Arria-Devoe' },
+    { id: 3, name: "Meat Lasagna", created_by: 'Guillermo Arria-Devoe' }
+  ]);
 };
